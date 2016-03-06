383d990266ed2a1fb29fbb89fa15fe43038339a7
Oleg Misyak <oleg.misyak@gmail.com>
2016-03-06 12:34:58 +0200

    some message

---
+++ tasks_olegmisyak.js
@@ -0,0 +1,38 @@
+{"status":200,
+"success":true,
+"result":[
+{
+"id":"1",
+"name":"coding"
+},
+
+{
+"id":"2",
+"name":"cooking"}]
+}
+
+
+{"status":200,
+"success":true,
+"result":[
+{
+"description":"code for an hour",
+"priority":"1",
+"project_id":"1",
+"status":"done"
+},
+{
+"description":"read a book",
+"priority":"2",
+"project_id":"1",
+"status":"done"
+},
+
+{
+"description":"cook something",
+"priority":"1",
+"project_id":"2",
+"status":"open"
+}
+]
+}
